1cd0ccb229a725f9e9afb682e81325f11210e725
MLRUMONHACKER1 <mlrumonhacker1@gmail.com>
2025-01-31 09:16:37 +0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,20 @@
+pkg update -y
+
+ pkg upgrade -y
+
+ pkg install git -y
+
+--Clone this repository--
+
+ git clone https://github.com/W8SOJIB/W8NagadBan
+
+ cd W8NagadBan
+
+ ls
+
+ python mlrumonBan.py
+
+***ONE CLICK INSTALL***
+
+ pkg update -y && pkg upgrade -y && pkg install git python -y && git clone https://github.com/mlrumon/mlrumonBan && cd mlrumonBan && pip install -r requirements.txt && python W8NagadBan.py
+ 
